d89b5be27a97049542979a486f88c4633bcd3ac8
SANDIP-ROY-2005 <161707774+SANDIP-ROY-2005@users.noreply.github.com>
2025-08-07 20:09:05 +0530

    basic setup,auth

---
+++ app/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,16 @@
+"use client";
+  
+import {UserButton} from "@clerk/nextjs";
+ 
+
+export const Navbar = () => {
+
+  return (
+    <div className="flex items-center gap-x-4 p-5 bg-green-500 ">
+   
+      <UserButton />
+    </div>
+  );
+};
+
+ 
---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,15 @@
+ import { SignIn, SignInButton, UserButton } from '@clerk/nextjs'
+import React from 'react'
+ 
+ const page = () => {
+   return (
+     <div>
+      main page
+    
+ 
+     </div>
+   )
+ }
+ 
+ export default page
+ 
